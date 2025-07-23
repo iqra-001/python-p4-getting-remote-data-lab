d64ea5113e4190cbf2ce64de3b226bdf90a5b387
iqra <abdikarimiqra@gmail.com>
2025-07-23 10:19:26 +0300

    assign

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,15 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+
+        response = requests.get(self.url)
+        return response.text
+        
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()
